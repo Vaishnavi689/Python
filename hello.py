d2d0f14a5413ca0f40efc6c4add59c5f065ecfee
Vaishnavi  Khaire <vaishnavikhaire110@gmail.com>
2025-11-17 09:45:29 +0530

    Day_1

---
+++ hello.py
@@ -0,0 +1,18 @@
+# # print is predefine function in python that output text to the console
+# # ''' triple code used for multicode line comment print("Hello everyone")
+# # this is comment
+print("Hello")
+
+# print function with multiple argument
+print("The sum of",10,"and",20,"is",10+20)
+print("The product of",5, "and",4,"is",5*4)
+print("The difference between", 20 , "and",6,"is",20-6)
+print("The quotient Of",20,"Divided by",4,"is",20/4)
+
+
+# Print function with F-string
+name="Vaishnavi"
+age=21
+address="Pune"
+salary=10000
+print("MY name is",name,"\n I am", age,"Year Old\n I live in",address,"\n My salary is", salary)
